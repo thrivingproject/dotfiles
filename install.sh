ef23eb68e0c8759293dfdce9a9f309d53c4b9c3c
Christian Isaman <christianisaman@outlook.com>
2024-04-20 20:52:29 +0800

    Update install.sh to kill current terminal session and open a new zsh session

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ ln -s ~/dotfiles/.aliases ~/.aliases
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 source ~/.zshrc
+# kill the current terminal session
+kill -9 $PPID
+# open a new zsh session
+zsh
 echo "Welcome to the dev env!"
