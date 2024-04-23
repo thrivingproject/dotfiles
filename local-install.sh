f2e297ecac484a477e2458cb3768df8917e8ed88
Christian Isaman <christianisaman@outlook.com>
2024-04-23 19:25:11 +0800

    set gitignore_global location

--- local-install.sh
+++ local-install.sh
@@ -1,5 +1,6 @@
 # #!/bin/bash
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 ln -s ~/.dotfiles/.aliases ~/.aliases
 rm ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
