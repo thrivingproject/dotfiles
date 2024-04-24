75665c9e897e88470ce419b2a76e87e262ca44cd
Christian Isaman <christianisaman@outlook.com>
2024-04-24 17:31:58 +0800

    remove gitconfig setup

--- local-install.sh
+++ local-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
-# git config --global core.excludesfile ~/.gitignore_global
 ln -s ~/.dotfiles/.aliases ~/.aliases
 rm ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
